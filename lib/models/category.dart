124220e2858727b786735cbf247fee4d694c4416
CodeByArslan <rajputarslan693@gmail.com>
2024-08-15 23:11:31 +0500

    Flutter basic Ui

---
+++ lib/models/category.dart
@@ -0,0 +1,36 @@
+class CategoryModel {
+   String vector;
+   bool isSelected;
+   CategoryModel({
+     required this.isSelected,
+     required this.vector,
+});
+   static List<CategoryModel> getCategories(){
+     List<CategoryModel> categories = [];
+     categories.add(
+       CategoryModel(
+         vector: 'assets/vectors/heart.svg',
+         isSelected: false
+       )
+     );
+     categories.add(
+         CategoryModel(
+             vector: 'assets/vectors/pil.svg',
+             isSelected: false
+         )
+     );
+     categories.add(
+         CategoryModel(
+             vector: 'assets/vectors/dentist.svg',
+             isSelected: true
+         )
+     );
+     categories.add(
+         CategoryModel(
+             vector: 'assets/vectors/pregnant.svg',
+             isSelected: false
+         )
+     );
+     return categories;
+   }
+}
